57b27fb281076a2a517d25c62b53d18a1b94de6e
Gui <74185763+ToGiFlip@users.noreply.github.com>
2022-10-02 01:33:00 +0200

    Updated: FieldOFView script (needsFixing)

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -11,13 +11,50 @@ public class FieldOfView : MonoBehaviour
     public LayerMask targetMask;
     public LayerMask obstacleMask;
 
+    [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
+
+    public float meshResolution;
+    public int edgeResolveIterations;
+    public float edgeDisTreshold;
+
+    public MeshFilter viewMeshFilter;
+    private Mesh viewMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewMesh = new Mesh();
+        viewMesh.name = "ViewMesh";
+        viewMeshFilter.mesh = viewMesh;
+        
+        StartCoroutine(FindTargetsWithDelay(0.2f));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        DrawFieldOfView();
+    }
+
+    // >----------------------------------------------COROUTINES-------------------------------------------------<
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+
+    // >-----------------------------------------------METHODS---------------------------------------------------<
 
     void FindVisibleTargets()
     {
         visibleTargets.Clear();
         Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
-        
+
         for (int i = 0; i < targetsInViewRadius.Length; i++)
         {
             Transform target = targetsInViewRadius[i].transform;
@@ -34,35 +71,152 @@ public class FieldOfView : MonoBehaviour
         }
     }
 
-    
+    void DrawFieldOfView()
+    {
+        //Number of Rays to cast inside viewAngle
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+
+        ViewCastInfo oldViewCast = new ViewCastInfo();
+
+        //Cast Rays evenly between angleStart and angleEnd
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            //Narrows down the position of obstacles corners
+            if (i > 0)
+            {
+                bool edgeDisTresholdExceeded = Mathf.Abs(oldViewCast.dis - newViewCast.dis) > edgeDisTreshold;
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDisTresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if(edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
+            //Adds to number of vertices in Mesh created below
+            viewPoints.Add(newViewCast.point);
+        }
+
+        //Mesh Info
+        int vertexCount = viewPoints.Count + 1;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3];
+
+        //Draws Mesh
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+            
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    EdgeInfo FindEdge(ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i = 0; i < edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = ViewCast(angle);
+
+            bool edgeDisTresholdExceeded = Mathf.Abs(minViewCast.dis - newViewCast.dis) > edgeDisTreshold;
+            if (newViewCast.hit == minViewCast.hit && !edgeDisTresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+
+        return new EdgeInfo(minPoint, maxPoint);
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+
+        //If it hits an obstacle
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        {
+            //Cast Rays along viewAngle until point it hits obstacle
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            //Cast Rays as normal
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+
     public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
         //sets angle to local (it wont change with character rotation)
-        if(!angleIsGlobal)
+        if (!angleIsGlobal)
         {
             angleInDegrees += transform.eulerAngles.y;
         }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(FindTargetsWithDelay(0.2f));
-    }
+    // >----------------------------------------------STRUCTS-------------------------------------------------<
 
-    // Update is called once per frame
-    void Update()
+    public struct ViewCastInfo
     {
-        
-    }
+        public bool hit;
+        public Vector3 point;
+        public float dis;
+        public float angle;
 
-    IEnumerator FindTargetsWithDelay(float delay)
-    {
-        while (true)
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dis, float _angle)
         {
-            yield return new WaitForSeconds(delay);
-            FindVisibleTargets();
+            hit = _hit;
+            point = _point;
+            dis = _dis;
+            angle = _angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 _pointA, Vector3 _pointB)
+        {
+            pointA = _pointA;
+            pointB = _pointB;
         }
     }
 }
