5db86b5753d9df0f1e90329acd1f6f1d677805a8
Gui <74185763+ToGiFlip@users.noreply.github.com>
2022-10-01 20:36:14 +0200

    Updated: PlayerMovement (HE MOVES)

--- Assets/PlayerMovement3D.cs
+++ Assets/PlayerMovement3D.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement3D : MonoBehaviour
 {
+    public float MovementSpeed;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * MovementSpeed, 0, verticalInput * MovementSpeed);
     }
 }
